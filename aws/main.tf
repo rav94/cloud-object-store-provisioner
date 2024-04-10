3d0f58f2fde0af369491c6560ddb3c62affa8555
rav94 <fernandoravindu1994@gmail.com>
2024-04-10 07:35:17 +0530

    Removing backend

--- aws/main.tf
+++ aws/main.tf
@@ -1,13 +1,13 @@
-terraform {
-    required_version = ">= 0.13"
-    backend "remote" {
-        hostname     = "app.terraform.io"
-        organization = "ravnf-personal"
-        workspaces {
-            name = "cloud-object-storage-aws"
-        }
-    }
-}
+# terraform {
+#     required_version = ">= 0.13"
+#     backend "remote" {
+#         hostname     = "app.terraform.io"
+#         organization = "ravnf-personal"
+#         workspaces {
+#             name = "cloud-object-storage-aws"
+#         }
+#     }
+# }
 
 module "aws_s3_bucket" {
     source = "github.com/rav94/demo-terraform-modules//aws_s3_bucket?ref=main"
